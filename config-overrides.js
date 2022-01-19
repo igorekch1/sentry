55450766c98b60f0cdf4d135295d90c6137f4a19
igorekch1 <igor.chernyaev1999@gmail.com>
2022-01-19 18:07:52 +0200

    Add sentry config options

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const { override } = require('customize-cra');
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 const addSentryPlugin = config => {
-  config.plugins.push(new SentryWebpackPlugin({
-    authToken: process.env.REACT_APP_SENTRY_AUTH_TOKEN,
-    org: process.env.REACT_APP_SENTRY_ORG,
-    project: process.env.REACT_APP_SENTRY_PROJECT,
-    release: process.env.REACT_APP_ENV
-  }));
+  if (process.env.NODE_ENV === 'production') {
+
+    config.plugins.push(new SentryWebpackPlugin({
+      authToken: process.env.REACT_APP_SENTRY_AUTH_TOKEN,
+      org: process.env.REACT_APP_SENTRY_ORG,
+      project: process.env.REACT_APP_SENTRY_PROJECT,
+      release: process.env.REACT_APP_ENV,
+      cleanArtifacts: true,
+      include: path.resolve(__dirname, 'build'),
+    }));
+  }
 
   return config;
 }
